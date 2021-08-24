be539e021f615318f4b19a168ee0f870d54bfdeb
Olka Korolchuk <olkakorolcuk@gmail.com>
2021-08-24 22:58:51 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome Olka2, to Your Vue.js App" />
+  <Button label="Submit" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Button from "primevue/button";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Button,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
